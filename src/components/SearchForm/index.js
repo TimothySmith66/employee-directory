b24e8ecceecf4d886ba4c963bf1a7062f916a4cd
Timothy Smith <timothytsmith8@gmail.com>
2021-02-18 07:19:57 -0800

    Added the search form.

--- src/components/SearchForm/index.js
+++ src/components/SearchForm/index.js
@@ -1,18 +1,18 @@
 import React from "react";
-
+import "./style.css";
 
 function SearchForm(props) {
   return (
     <form className="search">
       <div className="form-group">
-        <label htmlFor="language"></label>
+        <label htmlFor="language">Search Term:</label>
         <input
           value={props.search}
           onChange={props.handleInputChange}
           name="term"
           type="text"
           className="form-control"
-          placeholder="search"
+          placeholder="Type in an employee name."
           id="term"
         />
       </div>
