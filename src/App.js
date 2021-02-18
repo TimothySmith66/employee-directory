f3399389c6636d395cb71ee5053323b5f398ec52
Timothy Smith <timothytsmith8@gmail.com>
2021-02-18 07:25:59 -0800

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,16 @@
 import "./App.css";
+
 import Header from "./components/Header";
 import React from "react";
-import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 import Search from "./pages/Search";
-import Signup from "./pages/Signup";
-import Wrapper from "./components/Wrapper";
 
 const App = () => {
-  document.title = "Wikipedia Searcher";
+  document.title = "EmployeeLookup";
   return (
     <Router>
       <div>
         <Header />
-        <Wrapper>
-          <Route exact path="/">
-            <Redirect to="/search" />
-          </Route>
-          <Route exact path="/signup">
-            <Signup />
-          </Route>
-          <Route exact path="/search">
-            <Search />
-          </Route>
-        </Wrapper>
+            <Search /> 
       </div>
     </Router>
   );
